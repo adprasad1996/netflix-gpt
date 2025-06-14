d67ca9f249603b7a89b78cd3b5af0c82e417d794
adprasad1996 <adprasad1996@gmail.com>
2025-06-14 21:38:19 +0530

    Build and deploy: prepared dist for production

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/netflix-gpt/', // This is your GitHub repository name
-  build: {
-    outDir: 'dist', // Default output directory for Vite
-  },
+  plugins: [react()],
+  base: '/',
 });
